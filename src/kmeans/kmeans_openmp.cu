65eb5e7d06a65017f562cc190019021a78ee6e78
Dizy <dev@dizy.cc>
2022-11-20 18:44:03 -0500

    WIP kmeans openmp

--- src/kmeans/kmeans_openmp.cu
+++ src/kmeans/kmeans_openmp.cu
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    float A[100], B[100];
+    for (int i = 0; i < 100; i++) {
+        A[i] = 0;
+        B[i] = i;
+    }
+    float time_start = omp_get_wtime();
+#pragma omp target map(tofrom: A[0:100]) map(to: B[0:100])
+    {
+#pragma omp parallel for
+        for (int i = 0; i < 100; i++) {
+            A[i] = B[i];
+        }
+    }
+    float time_end = omp_get_wtime();
+    std::cout << "GPU Time: " << time_end - time_start << std::endl;
+
+    time_start = omp_get_wtime();
+    for (int i = 0; i < 100; i++) {
+        A[i] = B[i];
+    }
+    time_end = omp_get_wtime();
+    std::cout << "CPU Time: " << time_end - time_start << std::endl;
+
     return 0;
 }
