9c29d9064c979e9216ad6aad3ed23d41008f7f61
Dizy <dev@dizy.cc>
2022-11-20 18:53:21 -0500

    WIP kmeans openmp

--- src/kmeans/kmeans_openmp.cu
+++ src/kmeans/kmeans_openmp.cu
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <omp.h>
 
-#define N 1000000
+#define N 1000000000
 
 int main() {
-    float *A1, *B1;
-    A1 = (float *) malloc(N * sizeof(float));
-    B1 = (float *) malloc(N * sizeof(float));
+    double *A1, *B1;
+    A1 = (double *) malloc(N * sizeof(double));
+    B1 = (double *) malloc(N * sizeof(double));
     for (int i = 0; i < N; i++) {
         A1[i] = 0;
         B1[i] = i;
@@ -21,15 +21,15 @@ int main() {
     }
     float time_end = omp_get_wtime();
     std::cout << "GPU Time: " << time_end - time_start << std::endl;
-    float sum1 = 0;
+    double sum1 = 0;
     for (int i = 0; i < N; i++) {
         sum1 += A1[i];
     }
     std::cout << "GPU Sum: " << sum1 << std::endl;
 
-    float *A2, *B2;
-    A2 = (float *) malloc(N * sizeof(float));
-    B2 = (float *) malloc(N * sizeof(float));
+    double *A2, *B2;
+    A2 = (double *) malloc(N * sizeof(double));
+    B2 = (double *) malloc(N * sizeof(double));
     for (int i = 0; i < N; i++) {
         A2[i] = 0;
         B2[i] = i;
@@ -40,7 +40,7 @@ int main() {
     }
     time_end = omp_get_wtime();
     std::cout << "CPU Time: " << time_end - time_start << std::endl;
-    float sum2 = 0;
+    double sum2 = 0;
     for (int i = 0; i < N; i++) {
         sum2 += A2[i];
     }
