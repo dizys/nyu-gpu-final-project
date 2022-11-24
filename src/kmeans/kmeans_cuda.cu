fa430a850dd320ae5423153774b391d396da41c0
Dizy <dev@dizy.cc>
2022-11-24 18:13:42 -0500

    WIP

--- src/kmeans/kmeans_cuda.cu
+++ src/kmeans/kmeans_cuda.cu
@@ -177,7 +177,7 @@ int main(int argc, char *argv[])
     {
         kernel<<<grid_size, block_size>>>(vector_size, vector_stride, d_vectors, d_centroids, d_clusters, d_cluster_sizes, d_changed);
         cudaDeviceSynchronize();
-        // changed = false;
+        changed = true;
         cudaMemcpy(&changed, d_changed, sizeof(bool), cudaMemcpyDeviceToHost);
         iteration++;
         std::cout << "iteration " << iteration << ": " << (changed ? "changed" : "converged") << std::endl;
