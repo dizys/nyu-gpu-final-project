898681d187135105f6bed40417e5cd2a4e3d34e4
Dizy <dev@dizy.cc>
2022-11-24 18:26:11 -0500

    WIP

--- src/kmeans/kmeans_cuda.cu
+++ src/kmeans/kmeans_cuda.cu
@@ -175,7 +175,7 @@ int main(int argc, char *argv[])
     bool *d_changed;
     cudaMalloc((void **)&d_changed, sizeof(bool));
     int iteration = 0;
-    while (changed[0])
+    while (iteration < 1000)
     {
         kernel<<<grid_size, block_size>>>(vector_size, vector_stride, d_vectors, d_centroids, d_clusters, d_cluster_sizes, d_changed);
         cudaMemcpy(changed, d_changed, sizeof(bool), cudaMemcpyDeviceToHost);
