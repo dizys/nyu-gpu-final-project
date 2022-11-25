e540a405bf6840fc63e33e0bac684f109cf14510
Dizy <dev@dizy.cc>
2022-11-24 23:13:50 -0500

    WIP

--- src/kmeans/kmeans_cuda.cu
+++ src/kmeans/kmeans_cuda.cu
@@ -207,7 +207,7 @@ int main(int argc, char *argv[])
 
     int iteration = 0;
     std::cout << "stride: " << vector_stride << std::endl;
-    while (changed[0] && iteration < 100)
+    while (changed[0] && iteration < 10000)
     {
         cudaMemcpy(d_changed, changed, sizeof(bool), cudaMemcpyHostToDevice);
         kernel<<<grid_size, block_size>>>(vector_size, vector_stride, d_vectors, d_centroids, d_clusters, d_cluster_sizes, d_changed);
