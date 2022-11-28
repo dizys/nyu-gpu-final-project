ec6e7ea2f244c18fde495eb243c0dd2b03ce8692
Dizy <dev@dizy.cc>
2022-11-28 14:01:34 -0500

    WIP

--- src/bfs/bfs_seq.cpp
+++ src/bfs/bfs_seq.cpp
@@ -6,7 +6,7 @@
 #include <ctime>
 #include <stdio.h>
 
-bool **parse_input(const std::string &filename, long unsigned &sample_size, long unsigned &graph_size)
+bool **parse_input(const std::string &filename, int &sample_size, int &graph_size)
 {
   std::ifstream input;
   input.open(filename.c_str());
@@ -22,9 +22,9 @@ bool **parse_input(const std::string &filename, long unsigned &sample_size, long
   }
 
   bool **graph_list = (bool **)malloc(sample_size * sizeof(bool *));
-  for (long unsigned i = 0; i < sample_size; i++)
+  for (int i = 0; i < sample_size; i++)
   {
-    long unsigned new_graph_size;
+    int new_graph_size;
     if (!(input >> new_graph_size))
     {
       std::cout << "Error: cannot read graph size" << std::endl;
@@ -37,21 +37,21 @@ bool **parse_input(const std::string &filename, long unsigned &sample_size, long
     }
     graph_size = new_graph_size;
     graph_list[i] = (bool *)malloc(graph_size * graph_size * sizeof(bool));
-    for (long unsigned j = 0; j < graph_size * graph_size; j++)
+    for (int j = 0; j < graph_size * graph_size; j++)
     {
       graph_list[i][j] = false;
     }
-    for (long unsigned j = 0; j < graph_size; j++)
+    for (int j = 0; j < graph_size; j++)
     {
-      long unsigned list_size;
+      int list_size;
       if (!(input >> list_size))
       {
         std::cout << "Error: cannot read list size" << std::endl;
         exit(1);
       }
-      for (long unsigned k = 0; k < list_size; k++)
+      for (int k = 0; k < list_size; k++)
       {
-        long unsigned neighbor;
+        int neighbor;
         if (!(input >> neighbor))
         {
           std::cout << "Error: cannot read neighbor" << std::endl;
@@ -66,27 +66,27 @@ bool **parse_input(const std::string &filename, long unsigned &sample_size, long
   return graph_list;
 }
 
-void bfs_graph(bool *graph, long unsigned graph_size)
+void bfs_graph(bool *graph, int graph_size)
 {
   bool *visited = (bool *)malloc(graph_size * sizeof(bool));
-  for (long unsigned i = 0; i < graph_size; i++)
+  for (int i = 0; i < graph_size; i++)
   {
     visited[i] = false;
   }
   bool *explored = (bool *)malloc(graph_size * sizeof(bool));
-  for (long unsigned i = 0; i < graph_size; i++)
+  for (int i = 0; i < graph_size; i++)
   {
     explored[i] = false;
   }
-  long unsigned *queue = (long unsigned *)malloc(graph_size * sizeof(long unsigned));
-  long unsigned queue_start = 0;
-  long unsigned queue_end = 0;
+  int *queue = (int *)malloc(graph_size * sizeof(int));
+  int queue_start = 0;
+  int queue_end = 0;
   queue[queue_end++] = 0;
   explored[0] = true;
   while (queue_start < queue_end)
   {
-    long unsigned node = queue[queue_start++];
-    for (long unsigned i = 0; i < graph_size; i++)
+    int node = queue[queue_start++];
+    for (int i = 0; i < graph_size; i++)
     {
       if (graph[node * graph_size + i] && !explored[i])
       {
@@ -109,13 +109,13 @@ int main(int argc, char *argv[])
     exit(1);
   }
   std::string filename = argv[1];
-  long unsigned iterations = 1;
+  int iterations = 1;
   if (argc == 3)
   {
     iterations = atoi(argv[2]);
   }
-  long unsigned sample_size = 0;
-  long unsigned graph_size = 0;
+  int sample_size = 0;
+  int graph_size = 0;
   bool **graph_list = parse_input(filename, sample_size, graph_size);
   std::cout << "sample_size: " << sample_size << std::endl;
   std::cout << "graph_size: " << graph_size << std::endl;
@@ -123,9 +123,9 @@ int main(int argc, char *argv[])
   struct timespec start_time, end_time;
   clock_gettime(CLOCK_REALTIME, &start_time);
 
-  for (long unsigned i = 0; i < iterations; i++)
+  for (int i = 0; i < iterations; i++)
   {
-    for (long unsigned j = 0; j < sample_size; j++)
+    for (int j = 0; j < sample_size; j++)
     {
       bfs_graph(graph_list[j], graph_size);
       std::cout << "- sample " << j << "/" << sample_size << " done." << std::endl;
