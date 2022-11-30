db209566473f0bb0806b8fd94078eec18754c34a
Dizy <dev@dizy.cc>
2022-11-30 01:13:56 -0500

    WIP

--- src/bfs/bfs_cuda.cu
+++ src/bfs/bfs_cuda.cu
@@ -83,7 +83,7 @@ __global__ void bfs_kernel(bool *graph, bool *visited, bool *explored, int *fron
       {
         explored[j] = true;
         int index = atomicAdd(&next_frontier_size[0], 1);
-        next_frontier[0] = j;
+        next_frontier[index] = j;
       }
     }
   }
